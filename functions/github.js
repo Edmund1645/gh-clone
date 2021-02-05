45acc6137c33716f77b278395c07543c9fc209d4
Edmund Ekott <edmund.timfon@gmail.com>
2021-02-05 12:23:00 +0100

    use netlify functions

---
+++ functions/github.js
@@ -0,0 +1,60 @@
+const fetch = require('node-fetch');
+const baseUrl = 'https://api.github.com/graphql';
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+};
+exports.handler = function (event, context, callback) {
+  fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      Authorization: `bearer ${process.env.GH_TOKEN}`,
+    },
+    body: JSON.stringify({
+      query: `{ viewer {     login
+          starredRepositories {
+            totalCount
+          }
+          following {
+            totalCount
+          }
+          followers {
+            totalCount
+          }
+          repositories(last: 20) {
+            nodes {
+              name
+              url
+              description
+              updatedAt
+              isFork
+              stargazerCount
+              primaryLanguage {
+                name
+                color
+              }
+            }
+          }
+          avatarUrl
+          bio
+          email
+          name
+          location
+          twitterUsername
+          websiteUrl
+          organizations {
+            totalCount
+          } }}`,
+    }),
+  })
+    .then((response) => response.json())
+    .then(({ data }) =>
+      callback(null, {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify(data),
+      })
+    )
+    .catch((err) => console.log(err));
+};
